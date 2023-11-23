8f828d3b69e25d4c021ff1d621a2cad87fd9955c
Beaskyy <afeezakintobi@nqb8.co>
2023-11-23 21:41:09 +0100

    controllers

--- starter/routes/task.js
+++ starter/routes/task.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const { getAllTasks } = require("../controllers/task");
+const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.send("Get all items")
-})
+router.route("/").get(getAllTasks);
 
-module.exports = router
+module.exports = router;
