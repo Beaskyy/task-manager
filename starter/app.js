a2f4afd394af2a7c3b6709bd98baf3b0a270173b
Beaskyy <afeezakintobi@nqb8.co>
2023-11-23 21:28:41 +0100

    routes

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+const tasks = require("./routes/task")
+
+app.use("/api/v1/tasks", tasks)
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}...`)
+})
+
