e0dd747fea902ae960b79c1224cd25a830410e58
Beaskyy <afeezakintobi@nqb8.co>
2023-11-25 19:34:35 +0100

    update to task manager

--- starter/app.js
+++ starter/app.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const tasks = require("./routes/task")
+const tasks = require("./routes/task");
 
-app.use("/api/v1/tasks", tasks)
-app.use("/api/v1/tasks/:id", tasks)
+app.use(express.json());
 
-const port = 3000
+app.use("/api/v1/tasks", tasks);
+app.use("/api/v1/tasks/:id", tasks);
+
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`server listening on port ${port}...`)
-})
-
+  console.log(`server listening on port ${port}...`);
+});
