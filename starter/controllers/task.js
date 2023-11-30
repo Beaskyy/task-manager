1e51316936fc67bec205ecb621c02b06d0b211ed
Beaskyy <afeezakintobi@nqb8.co>
2023-11-30 16:40:35 +0100

    delete task

--- starter/controllers/task.js
+++ starter/controllers/task.js
@@ -33,12 +33,34 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.updateOne({ _id: taskId });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `cannot fin task with id: ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `cannot find task with id: ${taskId}` });
+    }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
